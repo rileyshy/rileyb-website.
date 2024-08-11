7bb7542f338ba0ac0eef2cd498b0b11a60364149
Riley Burton <140608020+rileyshy@users.noreply.github.com>
2024-08-11 02:59:24 +0100

    Update script.js

--- vortexsvs/script.js
+++ vortexsvs/script.js
@@ -36,7 +36,8 @@ renderPayPalButtons('paypal-button-container-script2', '30.00', 'Script 2', 'scr
 renderPayPalButtons('paypal-button-container-mlo1', '5.00', 'MLO 1', 'mlo1');
 renderPayPalButtons('paypal-button-container-mlo2', '2.00', 'MLO 2', 'mlo2');
 renderPayPalButtons('paypal-button-container-logo1', '5.00', 'Logo 1', 'logo1');
-renderPayPalButtons('paypal-button-container-logo2', '15.00', 'Logo 2', 'logo2');
+renderPayPalButtons('paypal-button-container-logo2', '2.00', 'Logo 2', 'logo2');
+renderPayPalButtons('paypal-button-container-logo3', '2.00', 'Logo 3', 'logo3');
 renderPayPalButtons('paypal-button-container-banner1', '12.00', 'Banner 1', 'banner1');
 renderPayPalButtons('paypal-button-container-banner2', '18.00', 'Banner 2', 'banner2');
 renderPayPalButtons('paypal-button-container-more1', '25.00', 'More Product 1', 'more1');
